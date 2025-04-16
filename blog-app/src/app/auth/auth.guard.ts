2363ff5f97fe65cb272091054f20bdbda4c940be
Kristi Rrapaj <74373101+kristirrapaj@users.noreply.github.com>
2025-04-16 09:44:09 +0200

    authentication guard

---
+++ blog-app/src/app/auth/auth.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+
+  canActivate(): boolean {
+    // controlla se l'utente è autenticato
+    if (this.auth.isLoggedIn()) {
+      return true; // si, consenti l'accesso alla rotta
+    } else {
+      this.router.navigate(['/login']); // reindirizza alla pagina di login
+      return false; // l'utente non è autenticato, blocca l'accesso alla rotta
+    }
+  }
+  
+}
