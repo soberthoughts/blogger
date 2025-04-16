4c4f44523fe18a1bd3e862fa9f4279850d78dbf7
Kristi Rrapaj <74373101+kristirrapaj@users.noreply.github.com>
2025-04-16 12:43:22 +0200

    fine front end

--- blog-app/src/app/posts/post-create/post-create.component.ts
+++ blog-app/src/app/posts/post-create/post-create.component.ts
@@ -4,7 +4,8 @@ import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
   title = '';
